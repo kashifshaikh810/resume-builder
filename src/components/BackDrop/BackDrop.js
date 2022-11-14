7250efd8c25037f920ff55dac1f1b78630c5ab2e
Kashif Sheikh <kashifshaikh810>
2022-11-14 19:12:03 +0500

    Partially implement backdrop and sidedrawer

---
+++ src/components/BackDrop/BackDrop.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const BackDrop = (props) => {
+  return (
+    <div className="backDrop">
+      <p>back</p>
+    </div>
+  );
+};
+
+export default BackDrop;
---
+++ src/components/SideDrawer/SideDrawer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const SideDrawer = (props) => {
+  return (
+    <div className="main-container">
+      <p>SideDrawer</p>
+    </div>
+  );
+};
+
+export default SideDrawer;
