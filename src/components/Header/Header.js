53d6ebf9eee38d92073c0f45c260b5e7411a232b
Kashif Sheikh <kashifshaikh810>
2022-11-14 19:11:24 +0500

    Header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,41 @@
 import React from "react";
-import Logo from "../images/logo.jpeg";
+import { FaRegAddressCard } from "react-icons/fa";
+import { BiChevronDown } from "react-icons/bi";
+import { AiOutlineMenu } from "react-icons/ai";
+import MyButton from "../MyButton/MyButton";
+import "./styles.css";
 
 const Header = (props) => {
   return (
-    <div className="flex flex-1 p-3 max-h-20 justify-center items-center bg-white">
-      <div className="flex flex-row items-center">
-        <img src={Logo} className="w-8/12 h-20" alt="png" />
+    <div className="flex flex-1 p-3 max-h-20 items-center bg-white sticky top-0 overflow-hidden z-0">
+      <div className="flex flex-row items-center mainContainer">
+        <div className="bg-gray-400 p-2 rounded-full">
+          <FaRegAddressCard size={25} color="#f3f3f3" />
+        </div>
       </div>
 
-      <div className="flex flex-1 flex-row items-center justify-center">
-        <p className="text-base pr-10">Resume</p>
-        <p className="text-base pr-10">Cover Letter</p>
-        <p className="text-base pr-10">Resume Writing</p>
+      <div className="flex flex-1 flex-row justify-end items-center mainContainer">
+        <p className="text-base">Resume</p>
+        <BiChevronDown size={15} color="black" className="mr-8 ml-1" />
+        <p className="text-base">Cover Letter</p>
+        <BiChevronDown size={15} color="black" className="mr-8 ml-1" />
+        <p className="text-base">Resume Writing</p>
+        <BiChevronDown size={15} color="black" className="mr-8 ml-1" />
         <p className="text-base pr-10">Blog</p>
         <p className="text-base pr-10">FAQ</p>
+        <div className="bg-gray-200 w-0.5 h-5 mr-10" />
+        <MyButton
+          {...props}
+          title="My Account"
+          className="border-2 border-blue-200 p-3 pl-5 pr-5 rounded-md cursor-pointer button"
+          textStyle="text-blue-400 font-bold"
+          onPress={() => {}}
+          loading={false}
+        />
+      </div>
+
+      <div>
+        <AiOutlineMenu size={40} className="icon icon-style" />
       </div>
     </div>
   );
