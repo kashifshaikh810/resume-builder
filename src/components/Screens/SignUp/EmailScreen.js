0b0720a859f79b406753518272408d1ccc767cf0
Kashif Sheikh <kashifshaikh810>
2022-11-16 15:05:00 +0500

    Implement email screen UI

--- src/components/Screens/SignUp/EmailScreen.js
+++ src/components/Screens/SignUp/EmailScreen.js
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "../../Header/Header";
+import MyButton from "../../MyButton/MyButton";
+import SideDrawer from "../../SideDrawer/SideDrawer";
+import "./styles.css";
 
-const EmailScreen = () => {
+const EmailScreen = (props) => {
+  const [toggleDrawer, setToggleDrawer] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const navigate = props.navigate();
+
+  const openDrawer = () => {
+    setToggleDrawer(!toggleDrawer);
+  };
+
   return (
     <div>
-      <p>EmailScreen</p>
+      <Header {...props} openDrawer={openDrawer} toggleDrawer={toggleDrawer} />
+      {toggleDrawer ? (
+        <SideDrawer
+          {...props}
+          openDrawer={openDrawer}
+          toggleDrawer={toggleDrawer}
+        />
+      ) : (
+        <></>
+      )}
+
+      <div className="flex flex-1 justify-center items-center mt-5 flex-col two-headings">
+        <p className="text-4xl font-bold text-blue-400">
+          Supply contact information
+        </p>
+        <p className="pt-5 w-6/12 text-center sub-title">
+          It’s important to let employers know how to contact you. Enter your
+          email address below.
+        </p>
+      </div>
+
+      <div className="flex flex-1 justify-center flex-col items-center pt-5">
+        <p className="w-6/12 pb-1 text-gray-500 text-sm input-heading">Email</p>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="bg-gray-100 w-6/12 h-10 pl-3 pr-3 input"
+        />
+      </div>
+
+      <div className="flex flex-row flex-1 justify-evenly mt-4 pt-4 buttons-container">
+        <MyButton
+          {...props}
+          title="Back"
+          className="border-2 border-gray-200 mt-4 p-3 pl-5 pr-5 rounded-md cursor-pointer back-button"
+          textStyle="text-black font-bold back-button"
+          onPress={() => navigate("/create-resume/introduction")}
+          loading={false}
+        />
+
+        <MyButton
+          {...props}
+          title="Continue"
+          className="bg-blue-500 mt-4 ml-12 p-3 pl-5 pr-5 rounded-md cursor-pointer continue-button"
+          textStyle="text-white font-bold"
+          onPress={() => {}}
+          loading={false}
+        />
+      </div>
     </div>
   );
 };
