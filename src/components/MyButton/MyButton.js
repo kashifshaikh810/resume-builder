319591dea3893bbaac9dbfd67725a9419d0e55d6
Kashif Sheikh <kashifshaikh810>
2022-11-16 18:59:51 +0500

    Add Loader to useable button

--- src/components/MyButton/MyButton.js
+++ src/components/MyButton/MyButton.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Spinner.css";
 
 const MyButton = (props) => {
   return (
     <div className={props?.className} onClick={props.onPress}>
       {props?.loading ? (
-        <p>Loading...</p>
+        <div className="loading-spinner" />
       ) : (
         <p className={props?.textStyle}>{props?.title}</p>
       )}
