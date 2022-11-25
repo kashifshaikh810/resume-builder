d40025170b305730e53d7115efa4d436968fd7ae
Kashif Sheikh <kashifshaikh810>
2022-11-25 16:14:35 +0500

    Implement account settings screen full responsive

--- src/components/Screens/AccountSettings/AccountSettingsMarkup.js
+++ src/components/Screens/AccountSettings/AccountSettingsMarkup.js
@@ -17,7 +17,7 @@ const AccountSettingsMarkup = (props) => {
       <DropDownMenu {...props} name="app" />
 
       <div className="m-auto bg-blue-50 flex-col main">
-        <div className="temp">
+        <div className="main-child">
           <div className="pl-6 max-[767px]:pl-7 max-[767px]:pt-5">
             <p className="text-2xl text-black font-bold">Account Settings</p>
           </div>
@@ -28,7 +28,7 @@ const AccountSettingsMarkup = (props) => {
           </div>
 
           <div className="mt-4 max-[767px]:w-full">
-            <div className="bg-white flex flex-row justify-center items-center w-full h-auto pt-5 pb-5 rounded-md pl-7 pr-5 mb-2 max-[767px]:w-full">
+            <div className="bg-white flex flex-row justify-center items-center w-full h-auto pt-5 pb-5 rounded-md pl-7 pr-6 mb-2 max-[767px]:w-full">
               <div className="bg-black w-10 h-10 pl-2 pr-2 rounded-full flex justify-center items-center mr-5 max-[767px]:ml-0">
                 <TfiInkPen size={20} color="#fff" />
               </div>
@@ -44,7 +44,7 @@ const AccountSettingsMarkup = (props) => {
                   {...props}
                   title="Upgrade"
                   className="bg-white mr-2 max-[767px]:mt-2"
-                  textStyle="text-blue-400"
+                  textStyle="text-blue-400 button-text"
                   onPress={(e) => {}}
                   loading={false}
                 />
@@ -57,7 +57,7 @@ const AccountSettingsMarkup = (props) => {
           </div>
 
           <div className="mt-2 max-[767px]:w-full">
-            <div className="bg-white w-full h-auto pt-5 pb-5 rounded-md pr-5 mb-7 max-[767px]:w-full max-[767px]:pl-4">
+            <div className="bg-white w-full h-auto pt-5 pb-5 rounded-md pr-2 mb-7 max-[767px]:w-full max-[767px]:pl-4">
               <div className="flex flex-row ml-7 max-[767px]:block max-[767px]:ml-2">
                 <div className="w-full max-[767px]:block">
                   <p className="text-sm text-gray-400 pb-2">First Name</p>
@@ -70,20 +70,20 @@ const AccountSettingsMarkup = (props) => {
                 </div>
               </div>
 
-              <div className="flex flex-row ml-7 mt-5 max-[767px]:block max-[767px]:ml-2">
+              <div className="flex flex-row mr-10 ml-7 mt-5 max-[767px]:block max-[767px]:ml-2 max-[767px]:mr-0">
                 <div className="w-full max-[767px]:block">
                   <p className="text-sm text-gray-400 pb-2">Email</p>
                   <input className="bg-gray-200 w-11/12 h-9 pl-4 pr-4 text-blue-400 border-none overflow-hidden outline-none max-[767px]:w-full" />
                 </div>
 
-                <div className="ml-8 w-full max-[767px]:ml-0">
+                <div className="ml-10 w-10/12 max-[767px]:ml-0">
                   <p className="text-sm text-gray-500 mt-6">
                     Use this email to log in to your Resume.io account and
                     receive notifications.
                   </p>
                 </div>
               </div>
-              <div className="flex flex-1 justify-end mt-4 mb-2 max-[767px]:block max-[767px]:ml-2">
+              <div className="flex flex-1 justify-end mt-4 mb-2 max-[767px]:block max-[767px]:ml-2 mr-2">
                 <MyButton
                   {...props}
                   title="Save"
@@ -166,19 +166,19 @@ const AccountSettingsMarkup = (props) => {
             </div>
           </div>
 
-          <div className="mt-5 max-[767px]:block max-[767px]:ml-3 max-[767px]:pl-4">
+          <div className="mt-5 pl-6 max-[767px]:ml-3 max-[767px]:pl-4">
             <p className="text-xs tracking-widest text-gray-400">
               EMAIL NOTIFICATIONS
             </p>
           </div>
 
-          <div className="mt-2 max-[767px]:block max-[767px]:w-full">
-            <div className="bg-white w-full h-auto pt-5 pb-5 mb-3 rounded-md pl-8 pr-8 max-[767px]:w-full max-[767px]:pr-2 max-[767px]:pl-0">
+          <div className="mt-2 max-[767px]:w-full">
+            <div className="bg-white w-full h-auto pt-5 pb-5 mb-3 rounded-md pl-6 pr-5 max-[767px]:w-full max-[767px]:pr-2 max-[767px]:pl-6">
               <div
                 className="flex flex-row cursor-pointer"
                 onClick={() => props?.setIsUpdate(!props?.isUpdate)}
               >
-                <div className="items-center pl-7 text-black">
+                <div className="items-center text-black">
                   <p>Updates and Offers</p>
                   <p className="text-xs text-gray-500">
                     Discounts, special offers, new features and more
@@ -201,7 +201,7 @@ const AccountSettingsMarkup = (props) => {
                 className="flex flex-row mt-5 cursor-pointer"
                 onClick={() => props?.setIsViews(!props?.isViews)}
               >
-                <div className="items-center pl-7 text-black">
+                <div className="items-center text-black">
                   <p>Resume Analytics</p>
                   <p className="text-xs text-gray-500">
                     Views, downloads and monthly statistics for each resume
@@ -224,7 +224,7 @@ const AccountSettingsMarkup = (props) => {
                 className="flex flex-row mt-5 cursor-pointer"
                 onClick={() => props?.setIsUseful(!props?.isUseful)}
               >
-                <div className="items-center pl-7 text-black">
+                <div className="items-center text-black">
                   <p>Resume and Job Tips Newsletter</p>
                   <p className="text-xs text-gray-500">
                     Useful resume and job tips! Straight to your inbox every 2
@@ -242,18 +242,18 @@ const AccountSettingsMarkup = (props) => {
               </div>
             </div>
           </div>
-          <div className="mt-4 ml-4 max-[767px]:block max-[767px]:pl-3">
+          <div className="mt-4 pl-6 max-[767px]:pl-6">
             <p className="text-xs tracking-widest text-gray-400">DANGER ZONE</p>
           </div>
 
-          <div className="flex flex-col w-11/12 justify-center items-center mt-4 max-[767px]:block max-[767px]:w-full">
-            <div className="bg-white flex flex-row justify-center items-center w-full h-auto pt-8 pb-8 mb-4 rounded-md pr-5 max-[767px]:flex-col max-[767px]:items-start max-[767px]:w-full max-[767px]:pl-0 max-[767px]:pr-0">
-              <p className="text-xs text-gray-400 pl-14 ml-1 max-[767px]:pl-6">
+          <div className="flex flex-col w-full justify-center items-center mt-4 max-[767px]:block max-[767px]:w-full">
+            <div className="bg-white flex flex-row justify-center items-center w-full h-auto pt-8 pb-8 mb-4 rounded-md pr-5 max-[767px]:flex-col max-[767px]:items-start max-[767px]:w-full max-[767px]:pr-0">
+              <p className="text-xs text-gray-400 pl-5 ml-1 max-[767px]:pl-5">
                 Once you delete your account, it cannot be undone. This is
                 permanent.
               </p>
 
-              <div className="flex flex-1 justify-end max-[767px]:ml-3 max-[767px]:pl-4 max-[767px]:mt-2">
+              <div className="flex flex-1 justify-end max-[767px]:ml-3 max-[767px]:pl-3 max-[767px]:mt-2">
                 <MyButton
                   {...props}
                   title="Delete Account"
@@ -264,14 +264,12 @@ const AccountSettingsMarkup = (props) => {
                 />
               </div>
             </div>
-            <p className="mb-10 w-5/12 text-gray-400 text-sm max-[767px]:w-7/12 max-[767px]:pb-4 max-[767px]:pl-4 max-[767px]:ml-2">
-              Need help? Have questions or feedback? Our team would love to hear
-              from you —{" "}
-              <b className="text-blue-500 cursor-pointer">
-                contact our support
-              </b>
-            </p>
           </div>
+          <p className="mb-10 pl-6 w-7/12 text-gray-400 text-sm max-[767px]:w-7/12 max-[767px]:pb-4 max-[767px]:pl-4 max-[767px]:ml-2">
+            Need help? Have questions or feedback? Our team would love to hear
+            from you —{" "}
+            <b className="text-blue-500 cursor-pointer">contact our support</b>
+          </p>
         </div>
       </div>
     </div>
