2a6fabac6046c08640e6ec563406b7a45fbe60ac
Kashif Sheikh <kashifshaikh810>
2022-11-28 19:07:20 +0500

    Implement responsive tabs and add dummy tabs data

--- src/components/Screens/ResumesTemplates/ResumesTemplates.js
+++ src/components/Screens/ResumesTemplates/ResumesTemplates.js
@@ -1,8 +1,209 @@
-import React from "react";
+import React, { useState } from "react";
 import ResumesTemplatesMarkup from "./ResumesTemplatesMarkup";
 
 const ResumesTemplates = (props) => {
-  return <ResumesTemplatesMarkup {...props} />;
+  const [showSelectedTab, setShowSelectedTab] = useState({
+    id: 0,
+    tabName: "All templates",
+  });
+  const [toggleDrawer, setToggleDrawer] = useState(false);
+  const navigate = props.navigate();
+
+  const openDrawer = () => {
+    setToggleDrawer(!toggleDrawer);
+  };
+
+  const creativeData = [
+    {
+      id: "0231",
+      name: "Vienna",
+      description:
+        "Striking modern header, professional two column template structure.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/406/persistent-resource/vienna-resume-templates.jpg",
+    },
+    {
+      id: "4232",
+      name: "Sydney",
+      description:
+        "Modern and eye-catching resume template. Beautiful contrasting structure.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/441/persistent-resource/sydney-resume-templates.jpg",
+    },
+    {
+      id: "3422",
+      name: "Moscow",
+      description: "Open white-space template, excellent readability.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/1523/persistent-resource/moscow-resume-templates.jpg",
+    },
+    {
+      id: "4232",
+      name: "Sydney",
+      description:
+        "Modern and eye-catching resume template. Beautiful contrasting structure.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/441/persistent-resource/sydney-resume-templates.jpg",
+    },
+    {
+      id: "4232",
+      name: "Sydney",
+      description:
+        "Modern and eye-catching resume template. Beautiful contrasting structure.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/441/persistent-resource/sydney-resume-templates.jpg",
+    },
+  ];
+
+  const allTempData = [
+    {
+      id: "0431",
+      name: "Toronto",
+      description:
+        "A web-inspired resume template perfect for chatting up your achievements.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/3501/persistent-resource/toronto-resume-templates.jpg",
+    },
+    {
+      id: "5553",
+      name: "Stockholm",
+      description:
+        "Perfect balance of fresh and functional resume template design.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/389/persistent-resource/new-york-resume-templates.jpg",
+    },
+    {
+      id: "9433",
+      name: "New York",
+      description:
+        "Professional and elegant resume template with a timeline structure.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/1523/persistent-resource/moscow-resume-templates.jpg",
+    },
+    {
+      id: "6666",
+      name: "London",
+      description:
+        "Classically structured resume template, for a robust career history.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/481/persistent-resource/london-resume-templates.jpg",
+    },
+  ];
+
+  const simpleData = [
+    {
+      id: "3424",
+      name: "London",
+      description:
+        "Classically structured resume template, for a robust career history.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/481/persistent-resource/london-resume-templates.jpg",
+    },
+    {
+      id: "4422",
+      name: "Santiago",
+      description:
+        "Classic full-page resume template with sizable resume sections.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/383/persistent-resource/santiago-resume-templates.jpg",
+    },
+    {
+      id: "3333",
+      name: "Singapore",
+      description: "Clean, orderly template structure with stylish minimalism.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/400/persistent-resource/singapore-resume-templates.jpg",
+    },
+    {
+      id: "5664",
+      name: "Oslo",
+      description:
+        "Reliable and elegant image. Minimalist single column outline.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/142/persistent-resource/oslo-resume-templates.jpg",
+    },
+  ];
+
+  const professionalData = [
+    {
+      id: "4552",
+      name: "Toronto",
+      description:
+        "A web-inspired resume template perfect for chatting up your achievements.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/3367/persistent-resource/toronto-resume-templates.jpg",
+    },
+    {
+      id: "6534",
+      name: "Stockholm",
+      description:
+        "Perfect balance of fresh and functional resume template design.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/370/persistent-resource/stockholm-resume-templates.jpg",
+    },
+    {
+      id: "3553",
+      name: "New York",
+      description:
+        "Professional and elegant resume template with a timeline structure.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/389/persistent-resource/new-york-resume-templates.jpg",
+    },
+    {
+      id: "5344",
+      name: "Vienna",
+      description:
+        "Striking modern header, professional two column template structure.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/407/persistent-resource/vienna-resume-templates.jpg",
+    },
+    {
+      id: "5332",
+      name: "Dublin",
+      description:
+        "A touch of personality with a well-organized resume structure.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/488/persistent-resource/dublin-resume-templates.jpg",
+    },
+  ];
+
+  const modernData = [
+    {
+      id: "4552",
+      name: "Toronto",
+      description:
+        "A web-inspired resume template perfect for chatting up your achievements.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/3367/persistent-resource/toronto-resume-templates.jpg",
+    },
+    {
+      id: "6534",
+      name: "Stockholm",
+      description:
+        "Perfect balance of fresh and functional resume template design.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/370/persistent-resource/stockholm-resume-templates.jpg",
+    },
+    {
+      id: "3424",
+      name: "London",
+      description:
+        "Classically structured resume template, for a robust career history.",
+      cvImage:
+        "https://s3.resume.io/cdn-cgi/image/width=380,format=auto/uploads/local_template_image/image/481/persistent-resource/london-resume-templates.jpg",
+    },
+  ];
+
+  return (
+    <ResumesTemplatesMarkup
+      {...props}
+      showSelectedTab={showSelectedTab}
+      setShowSelectedTab={setShowSelectedTab}
+      openDrawer={openDrawer}
+      navigate={navigate}
+      toggleDrawer={toggleDrawer}
+    />
+  );
 };
 
 export default ResumesTemplates;
--- src/components/Screens/ResumesTemplates/ResumesTemplatesMarkup.js
+++ src/components/Screens/ResumesTemplates/ResumesTemplatesMarkup.js
@@ -6,6 +6,8 @@ import { FaRegCopyright } from "react-icons/fa";
 import { BsStars } from "react-icons/bs";
 import { TbBriefcase } from "react-icons/tb";
 import { TbHandRock } from "react-icons/tb";
+import SideDrawer from "../../../components/SideDrawer/SideDrawer";
+import "./styles.css";
 
 const ResumesTemplatesMarkup = (props) => {
   const tabsData = [
@@ -19,11 +21,13 @@ const ResumesTemplatesMarkup = (props) => {
     },
     { id: 4, tabName: "Modern", tabIcon: () => <TbHandRock size={25} /> },
   ];
+
   return (
     <div>
       <Header {...props} />
+      {props?.toggleDrawer && <SideDrawer {...props} />}
 
-      <div className="m-auto container">
+      <div className={`m-auto ${`max-[767px]:` ? `` : `container`}`}>
         <div className="container-main">
           <div className="flex flex-col w-full justify-center items-center mt-5">
             <p className="text-3xl text-center font-bold max-[767px]:text-2xl">
@@ -45,19 +49,87 @@ const ResumesTemplatesMarkup = (props) => {
             />
           </div>
 
-          <div className="flex flex-row justify-center mt-5 border-b border-gray-200">
+          <div className="flex flex-row justify-center max-[767px]:justify-start mt-5 border-b border-gray-200 overflow-auto">
             {tabsData.map((item, index) => (
               <div
-                className="flex flex-row items-center p-5 border-b border-blue-200 tab-container"
+                className={`flex flex-row items-center p-5 max-[1023px]:pl-1 pb-0 ${
+                  (props?.showSelectedTab.tabName === "All templates" &&
+                    props?.showSelectedTab.id === index) ||
+                  (props?.showSelectedTab.tabName === "Creative" &&
+                    props?.showSelectedTab.id === index) ||
+                  (props?.showSelectedTab.tabName === "Simple" &&
+                    props?.showSelectedTab.id === index) ||
+                  (props?.showSelectedTab.tabName === "Professional" &&
+                    props?.showSelectedTab.id === index) ||
+                  (props?.showSelectedTab.tabName === "Modern" &&
+                    props?.showSelectedTab.id === index)
+                    ? `cursor-default`
+                    : `tab-container`
+                }`}
                 key={index}
+                onClick={() => props?.setShowSelectedTab(item)}
               >
-                <div className="text-gray-400">{item.tabIcon()}</div>
-                <p className="ml-2 font-bold text-gray-400 tab-text">
-                  {item.tabName}
-                </p>
+                <div
+                  className={`flex flex-row pb-5 max-[1023px]:pb-2 ${
+                    (props?.showSelectedTab.tabName === "All templates" &&
+                      props?.showSelectedTab.id === index) ||
+                    (props?.showSelectedTab.tabName === "Creative" &&
+                      props?.showSelectedTab.id === index) ||
+                    (props?.showSelectedTab.tabName === "Simple" &&
+                      props?.showSelectedTab.id === index) ||
+                    (props?.showSelectedTab.tabName === "Professional" &&
+                      props?.showSelectedTab.id === index) ||
+                    (props?.showSelectedTab.tabName === "Modern" &&
+                      props?.showSelectedTab.id === index)
+                      ? `border-b-2 border-blue-400`
+                      : ``
+                  }`}
+                >
+                  <div
+                    className={`text-gray-400 tab-icon max-[1023px]:hidden ${
+                      (props?.showSelectedTab.tabName === "All templates" &&
+                        props?.showSelectedTab.id === index) ||
+                      (props?.showSelectedTab.tabName === "Creative" &&
+                        props?.showSelectedTab.id === index) ||
+                      (props?.showSelectedTab.tabName === "Simple" &&
+                        props?.showSelectedTab.id === index) ||
+                      (props?.showSelectedTab.tabName === "Professional" &&
+                        props?.showSelectedTab.id === index) ||
+                      (props?.showSelectedTab.tabName === "Modern" &&
+                        props?.showSelectedTab.id === index)
+                        ? `text-blue-400`
+                        : ``
+                    }`}
+                  >
+                    {item.tabIcon()}
+                  </div>
+                  <p
+                    className={`ml-2 text-sm max-[1023px]:ml-0 font-bold text-gray-400 tracking-widest max-[1023px]:text-xs max-[1023px]:uppercase tab-text max-[375px]:h-7 ${
+                      (props?.showSelectedTab.tabName === "All templates" &&
+                        props?.showSelectedTab.id === index) ||
+                      (props?.showSelectedTab.tabName === "Creative" &&
+                        props?.showSelectedTab.id === index) ||
+                      (props?.showSelectedTab.tabName === "Simple" &&
+                        props?.showSelectedTab.id === index) ||
+                      (props?.showSelectedTab.tabName === "Professional" &&
+                        props?.showSelectedTab.id === index) ||
+                      (props?.showSelectedTab.tabName === "Modern" &&
+                        props?.showSelectedTab.id === index)
+                        ? `text-black`
+                        : ``
+                    }`}
+                  >
+                    {item.tabName}
+                  </p>
+                </div>
               </div>
             ))}
           </div>
+          {/* {dummyData.map((item, i) => (
+            <div>
+              <img src={item.cvImage} className="w-5/12 h-auto" />
+            </div>
+          ))} */}
         </div>
       </div>
     </div>
