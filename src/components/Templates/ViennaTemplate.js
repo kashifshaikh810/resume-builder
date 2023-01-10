09e650ad73ee5945810343827d60c8e3f0819652
Kashif Sheikh <kashifshaikh810>
2023-01-10 15:47:51 +0500

    Just create page 2 on vienna template

--- src/components/Templates/ViennaTemplate.js
+++ src/components/Templates/ViennaTemplate.js
@@ -1,26 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { MdOutlineAttachment } from "react-icons/md";
 
 import "./styles.css";
 
 const ViennaTemplate = (props) => {
-  return (
-    <div
-      className="flex flex-col w-full h-[930px] 
-      border-l border-gray-600 ml-5
-      max-[1029px]:pl-1 pl-10 pb-10 
-      unselectable
-      max-[768px]:border-none
-      max-[768px]:ml-0
-      max-[620px]:h-[900px]
-      max-[703px]:h-[880px]
-      max-[610px]:h-[830px]
-      max-[570px]:h-[780px]
-      max-[535px]:h-[730px]
-      max-[500px]:h-[680px]
-      "
-    >
-      <div className="flex bg-white w-full max-[1029px]:w-11/12 overflow-hidden h-[900px] self-center rounded-md hover:default">
+  useEffect(() => {
+    props.setTotalPage(2);
+  }, [props]);
+
+  const pageOne = () => {
+    if (props.page === 1) {
+      return (
         <div className="p-10 w-full">
           <div className="flex flex-row w-full">
             <img
@@ -52,10 +42,10 @@ const ViennaTemplate = (props) => {
 
           <div
             className="
-      grid 
-      grid-cols-[250px_minmax(250px,_1fr)_100px]
-       mt-4 pt-4
-      "
+    grid 
+    grid-cols-[250px_minmax(250px,_1fr)_100px]
+     mt-4 pt-4
+    "
           >
             <div>
               <div className="flex flex-row items-center">
@@ -127,6 +117,42 @@ const ViennaTemplate = (props) => {
             </div>
           </div>
         </div>
+      );
+    }
+  };
+
+  const pageTwo = () => {
+    if (props.page === 2) {
+      return (
+        <div>
+          <p>pageTwo</p>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div
+      className="flex flex-col w-full h-[930px] 
+      border-l border-gray-600 ml-5
+      max-[1029px]:pl-1 pl-10 pb-10 
+      unselectable
+      max-[768px]:border-none
+      max-[768px]:ml-0
+      max-[620px]:h-[900px]
+      max-[703px]:h-[880px]
+      max-[610px]:h-[830px]
+      max-[570px]:h-[780px]
+      max-[535px]:h-[730px]
+      max-[500px]:h-[680px]
+      "
+    >
+      <div className="flex bg-white w-full max-[1029px]:w-11/12 overflow-hidden h-[900px] self-center rounded-md hover:default">
+        {/* pageOne */}
+        {pageOne()}
+
+        {/* pageTwo */}
+        {pageTwo()}
       </div>
     </div>
   );
