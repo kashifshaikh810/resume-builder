3cc6efbae348f81e1e5214adc8ca5c8f520998cf
Kashif Sheikh <kashifshaikh810>
2023-01-23 15:56:50 +0500

    Implement details section in NewYorkTemplate in EditResume screen

--- src/components/EditResumeTemplates/NewYorkTemplate.js
+++ src/components/EditResumeTemplates/NewYorkTemplate.js
@@ -14,6 +14,20 @@ import "./styles.css";
 const NewYorkTemplate = (props) => {
   const { resumeData } = props;
 
+  const levelWidth = {
+    Novice: "w-[15px]",
+    Beginner: "w-[25px]",
+    Skillful: "w-[37px]",
+    Experienced: "w-[52px]",
+    Expert: "w-[65px]",
+  };
+
+  const languageLevelWidth = {
+    "Very good command": "w-[40px]",
+    "Highly Proficient": "w-[53px]",
+    "Native Speaker": "w-[65px]",
+  };
+
   // useEffect(() => {
   //   props.setTotalPage(2);
   // }, [props]);
@@ -47,16 +61,32 @@ const NewYorkTemplate = (props) => {
                     : `mt-7`
                 }`}
               >
-                <p className="text-[4px] uppercase">WEB DEVELOPER</p>
+                <p className="text-[4px] uppercase">
+                  {resumeData?.wantedJobTitle}
+                </p>
                 <div className="flex flex-row items-center ml-1.5">
                   <MdLocationOn className="text-[4px]" />
-                  <p className="text-[4px] ml-0.5 uppercase">wwwwwww, </p>
-                  <p className="text-[4px] ml-0.5 uppercase">ffreerf, </p>
-                  <p className="text-[4px] ml-0.5 uppercase">CYCAD </p>
+                  <p className="text-[4px] ml-0.5 uppercase">
+                    {resumeData?.city}
+                    {resumeData?.postalCode || resumeData?.city ? `,` : ``}{" "}
+                  </p>
+                  <p className={`text-[4px] ml-0.5 uppercase`}>
+                    {resumeData?.postalCode}
+                    {resumeData?.postalCode ? `,` : ``}
+                  </p>
+                  <p
+                    className={`text-[4px] ${
+                      resumeData?.postalCode && resumeData?.country
+                        ? `ml-0.5`
+                        : ``
+                    } uppercase`}
+                  >
+                    {resumeData?.country}{" "}
+                  </p>
                 </div>
                 <div className="flex flex-row items-center ml-1.5">
                   <BsTelephoneFill className="text-[4px]" />
-                  <p className="text-[4px] ml-0.5"> 03235422343 </p>
+                  <p className="text-[4px] ml-0.5"> {resumeData?.phone} </p>
                 </div>
               </div>
             </div>
@@ -88,94 +118,178 @@ const NewYorkTemplate = (props) => {
               </div>
 
               <div className="mt-0.5 flex flex-col items-center">
-                <p className="text-[5px]">bgrjknje</p>
-                <p className="text-[5px]">wwweewe, 2042</p>
-                <p className="text-[5px]">cdwcsf</p>
-                <p className="text-[5px]">0313423442</p>
-                <p className="text-[5px] underline">
-                  kashifshaikh910ssg@gmail.com
+                <p className="text-[5px]">{resumeData?.address}</p>
+                <p className="text-[5px]">
+                  {resumeData?.city}
+                  {resumeData?.city && resumeData?.postalCode ? `,` : ``}{" "}
+                  {resumeData?.postalCode}
                 </p>
+                <p className="text-[5px]">{resumeData?.country}</p>
+                <p className="text-[5px]">{resumeData?.phone}</p>
+                <p className="text-[5px] underline">{resumeData?.email}</p>
               </div>
 
-              <div className="mt-1 flex flex-col items-center">
-                <p className="text-[5px] text-gray-500">
-                  Date / Place of birth
-                </p>
-                <p className="text-[5px]">23</p>
-                <p className="text-[5px]">2421</p>
-              </div>
-
-              <div className="mt-1 flex flex-col items-center">
-                <p className="text-[5px] text-gray-500">Nationality</p>
-                <p className="text-[5px]">322</p>
-              </div>
-
-              <div className="mt-1 flex flex-col items-center">
-                <p className="text-[5px] text-gray-500">Driving license</p>
-                <p className="text-[5px]">423</p>
-              </div>
-
-              <div className="flex flex-col justify-center items-center mt-2">
-                <div className="flex flex-row justify-center items-center">
-                  <CgShapeCircle className="text-[4px] mr-1.5" />
-                  <p className="uppercase text-[6px] font-[600] -tracking-[0.5px]">
-                    Links
+              {resumeData?.dateOfBirth || resumeData?.placeOfBirth ? (
+                <div className="mt-1 flex flex-col items-center">
+                  <p className="text-[5px] text-gray-500">
+                    Date / Place of birth
                   </p>
-                  <CgShapeCircle className="text-[4px] ml-1.5" />
+                  <p className="text-[5px]">{resumeData?.dateOfBirth}</p>
+                  <p className="text-[5px]">{resumeData?.placeOfBirth}</p>
                 </div>
-                <p className="text-[5px] mt-1.5 underline">greijgijrr</p>
-              </div>
+              ) : (
+                ""
+              )}
 
-              <div className="flex flex-col justify-center items-center mt-2">
-                <div className="flex flex-row justify-center items-center">
-                  <CgShapeCircle className="text-[4px] mr-1.5" />
-                  <p className="uppercase text-[6px] font-[600] -tracking-[0.5px]">
-                    Skills
-                  </p>
-                  <CgShapeCircle className="text-[4px] ml-1.5" />
+              {resumeData?.nationality ? (
+                <div className="mt-1 flex flex-col items-center">
+                  <p className="text-[5px] text-gray-500">Nationality</p>
+                  <p className="text-[5px]">{resumeData?.nationality}</p>
                 </div>
-                <div>
-                  <p className="text-[5px] mt-1.5 text-center">Communication</p>
-                  <div className="w-[65px] bg-gray-300" id="myProgress">
-                    {/* 6 || 8 || 10*/}
-                    <div
-                      className="w-[30px] h-[1.5px] bg-gray-800 mt-1"
-                      id="myBar"
-                    ></div>
+              ) : (
+                ""
+              )}
+
+              {resumeData?.drivingLicense ? (
+                <div className="mt-1 flex flex-col items-center">
+                  <p className="text-[5px] text-gray-500">Driving license</p>
+                  <p className="text-[5px]">{resumeData?.drivingLicense}</p>
+                </div>
+              ) : (
+                ""
+              )}
+
+              {resumeData?.websiteInputList?.length >= 1 ? (
+                <div className="flex flex-col justify-center items-center mt-2">
+                  <div className="flex flex-row justify-center items-center">
+                    <CgShapeCircle className="text-[4px] mr-1.5" />
+                    <p className="uppercase text-[6px] font-[600] -tracking-[0.5px]">
+                      {props?.websiteInput === "Websites & Social Links"
+                        ? "Links"
+                        : props?.websiteInput}
+                    </p>
+                    <CgShapeCircle className="text-[4px] ml-1.5" />
+                  </div>
+                  {resumeData?.websiteInputList &&
+                    resumeData?.websiteInputList.map((item, index) => {
+                      return (
+                        <div key={index}>
+                          <p className="text-[5px] mt-0.5 underline">
+                            {item?.label}
+                          </p>
+                        </div>
+                      );
+                    })}
+                </div>
+              ) : (
+                ""
+              )}
+
+              {resumeData?.skillsInputList?.length >= 1 ? (
+                <div className="flex flex-col justify-center items-center mt-2">
+                  <div className="flex flex-row justify-center items-center">
+                    <CgShapeCircle className="text-[4px] mr-1.5" />
+                    <p className="uppercase text-[6px] font-[600] -tracking-[0.5px]">
+                      {props?.skillsInput}
+                    </p>
+                    <CgShapeCircle className="text-[4px] ml-1.5" />
+                  </div>
+                  <div>
+                    {resumeData.skillsInputList &&
+                      resumeData.skillsInputList.map((item, index) => {
+                        return (
+                          <div key={index}>
+                            <p className="text-[5px] mt-1.5 text-center">
+                              {item?.skill}
+                            </p>
+                            {resumeData?.isNotShowExpertLevel === false ? (
+                              <div
+                                className="w-[65px] bg-gray-300"
+                                id="myProgress"
+                              >
+                                {/* 15 || 25 || 37 || 52 || 65 */}
+                                <div
+                                  className={`${
+                                    levelWidth[item?.level]
+                                  } h-[1.5px] bg-gray-800 mt-1`}
+                                  id="myBar"
+                                ></div>
+                              </div>
+                            ) : (
+                              ""
+                            )}
+                          </div>
+                        );
+                      })}
                   </div>
                 </div>
-              </div>
+              ) : (
+                ""
+              )}
 
-              <div className="flex flex-col justify-center items-center mt-2">
-                <div className="flex flex-row justify-center items-center">
-                  <CgShapeCircle className="text-[4px] mr-1.5" />
-                  <p className="uppercase text-[6px] font-[600] -tracking-[0.5px]">
-                    Hobbies
+              {resumeData?.hobbies ? (
+                <div className="flex flex-col justify-center items-center mt-2">
+                  <div className="flex flex-row justify-center items-center">
+                    <CgShapeCircle className="text-[4px] mr-1.5" />
+                    <p className="uppercase text-[6px] font-[600] -tracking-[0.5px]">
+                      Hobbies
+                    </p>
+                    <CgShapeCircle className="text-[4px] ml-1.5" />
+                  </div>
+                  <p className="text-[5px] mt-0.5 p-4 pt-0 pb-0">
+                    {resumeData?.hobbies}
                   </p>
-                  <CgShapeCircle className="text-[4px] ml-1.5" />
                 </div>
-                <p className="text-[5px] mt-1.5">rgerr</p>
-              </div>
+              ) : (
+                ""
+              )}
 
-              <div className="flex flex-col justify-center items-center mt-2 w-full">
-                <div className="flex flex-row justify-center items-center">
-                  <CgShapeCircle className="text-[4px] mr-1.5" />
-                  <p className="uppercase text-[6px] font-[600] -tracking-[0.5px]">
-                    Languages
-                  </p>
-                  <CgShapeCircle className="text-[4px] ml-1.5" />
-                </div>
-                <div>
-                  <p className="text-[5px] mt-1.5 text-center">rgerr</p>
-                  <div className="w-[65px] bg-gray-300" id="myProgress">
-                    {/* 6 || 8 || 10*/}
-                    <div
-                      className="w-[30px] h-[1.5px] bg-gray-800 mt-1"
-                      id="myBar"
-                    ></div>
+              {resumeData?.languagesInputList?.length >= 1 ? (
+                <div className="flex flex-col justify-center items-center mt-2 w-full">
+                  <div className="flex flex-row justify-center items-center">
+                    <CgShapeCircle className="text-[4px] mr-1.5" />
+                    <p className="uppercase text-[6px] font-[600] -tracking-[0.5px]">
+                      {props?.languagesInput}
+                    </p>
+                    <CgShapeCircle className="text-[4px] ml-1.5" />
+                  </div>
+                  <div>
+                    {resumeData?.languagesInputList &&
+                      resumeData?.languagesInputList.map((item, index) => {
+                        return (
+                          <div key={index}>
+                            {item?.language ? (
+                              <p className="text-[5px] mt-0.5 text-center">
+                                {item?.language}
+                              </p>
+                            ) : (
+                              ""
+                            )}
+                            {!item?.language ||
+                            item?.level === "Select Level" ? (
+                              ""
+                            ) : (
+                              <div
+                                className="w-[65px] bg-gray-300"
+                                id="myProgress"
+                              >
+                                {/* 40 || 53 || 65 */}
+                                <div
+                                  className={`${
+                                    languageLevelWidth[item?.level]
+                                  } h-[1.5px] bg-gray-800 mt-1`}
+                                  id="myBar"
+                                ></div>
+                              </div>
+                            )}
+                          </div>
+                        );
+                      })}
                   </div>
                 </div>
-              </div>
+              ) : (
+                ""
+              )}
             </div>
 
             {/* Column 2 */}
