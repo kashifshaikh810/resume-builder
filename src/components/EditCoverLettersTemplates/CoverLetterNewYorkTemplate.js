a0905f86948965a77368143ec2861e7ce760fb10
Kashif Sheikh <kashifshaikh810@github.com>
2023-02-17 16:12:54 +0500

    Implement done coverLetterNowYorkTemplate screen in EditResume screen

--- src/components/EditCoverLettersTemplates/CoverLetterNewYorkTemplate.js
+++ src/components/EditCoverLettersTemplates/CoverLetterNewYorkTemplate.js
@@ -14,24 +14,36 @@ const CoverLetterNewYorkTemplate = (props) => {
 
   return (
     <div className="m-6 w-full">
-      <div className="grid grid-cols-[200px_minmax(900px,_1fr)_100px]">
-        <div>
-          <p>Muhammad Kashif</p>
-          <p className="text-[5px]">1</p>
+      <div className="grid grid-cols-[150px_minmax(220px,_1fr)_100px] -space-x-[3.2rem]">
+        <div className="w-7/12">
+          <div className="flex flex-col items-end">
+            <p className="text-[10px] font-semibold text-end uppercase">
+              {fullName}
+            </p>
+            <p className="text-[5px] uppercase mt-1">{jobTitle}</p>
+          </div>
+
+          <div className="flex flex-col items-end mt-6">
+            <p className="text-[5px] text-black font-bold">
+              {hiringManagerName || companyName ? `To` : ``}
+            </p>
+            <p className="text-[5px]">{hiringManagerName}</p>
+            <p className="text-[5px]">{companyName}</p>
+          </div>
+
+          <div className="flex flex-col items-end mt-6">
+            <p className="text-[5px] text-black font-bold">
+              {fullName || jobTitle || address || email || phone ? `From` : ``}
+            </p>
+            <p className="text-[5px]">{fullName}</p>
+            <p className="text-[5px]">{jobTitle}</p>
+            <p className="text-[5px]">{address}</p>
+            <p className="text-[5px]">{phone}</p>
+            <p className="text-[5px] underline">{email}</p>
+          </div>
         </div>
-        <div className="w-[1px] h-[29rem] bg-black relative -left-12" />
-        <div className="w-full relative -left-[9rem]">
-          <p className="text-[5px]">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
+        <div className="w-full h-[28rem] border-l border-black pl-3.5 pt-[2.1rem]">
+          <p className="text-[5px] leading-4">{letterDetails}</p>
         </div>
       </div>
     </div>
