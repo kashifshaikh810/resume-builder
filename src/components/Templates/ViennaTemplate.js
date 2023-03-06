e499e4a44c3beb785d53aa4763b5ef3153eab3a4
Kashif Sheikh <kashifshaikh810@github.com>
2023-03-06 17:17:31 +0500

    Implement add condition in ViennaTemplate

--- src/components/Templates/ViennaTemplate.js
+++ src/components/Templates/ViennaTemplate.js
@@ -125,21 +125,29 @@ const ViennaTemplate = (props) => {
                 ""
               )}
 
-              <div className="mt-5">
-                <p className="text-[12px] font-bold">Date / Place of birth</p>
-                <p className="text-[12px]">
-                  {dateOfBirth}
-                  {dateOfBirth && placeOfBirth ? `,` : ``} {placeOfBirth}
-                </p>
-              </div>
-              <div className="mt-5">
-                <p className="text-[12px] font-bold">Nationality</p>
-                <p className="text-[12px]">{nationality}</p>
-              </div>
-              <div className="mt-5">
-                <p className="text-[12px] font-bold">Driving License</p>
-                <p className="text-[12px]">{drivingLicense}</p>
-              </div>
+              {dateOfBirth || placeOfBirth ? (
+                <div className="mt-5">
+                  <p className="text-[12px] font-bold">Date / Place of birth</p>
+                  <p className="text-[12px]">
+                    {dateOfBirth}
+                    {dateOfBirth && placeOfBirth ? `,` : ``} {placeOfBirth}
+                  </p>
+                </div>
+              ) : (
+                ``
+              )}
+              {nationality && (
+                <div className="mt-5">
+                  <p className="text-[12px] font-bold">Nationality</p>
+                  <p className="text-[12px]">{nationality}</p>
+                </div>
+              )}
+              {drivingLicense && (
+                <div className="mt-5">
+                  <p className="text-[12px] font-bold">Driving License</p>
+                  <p className="text-[12px]">{drivingLicense}</p>
+                </div>
+              )}
 
               <div className="mt-5">
                 <p className="text-[12px] font-bold">Skills</p>
