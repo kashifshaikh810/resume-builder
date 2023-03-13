4d70500ceb999e0407c760f15ec0439f61c698f7
Kashif Sheikh <kashifshaikh810@github.com>
2023-03-13 15:49:38 +0500

    Implement UI changes fixes

--- src/components/Screens/Dashboard/DashboardMarkup.js
+++ src/components/Screens/Dashboard/DashboardMarkup.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import Header from "../../Header/Header";
 import MyButton from "../../MyButton/MyButton";
-import CVImage from "../../images/cvImage.png";
 import SideDrawer from "../../SideDrawer/SideDrawer";
+import "./styles.css";
 
 const DashboardMarkup = (props) => {
   const { loading } = props;
@@ -21,11 +21,13 @@ const DashboardMarkup = (props) => {
 
       {props?.toggleDrawer ? <SideDrawer {...props} /> : <></>}
       <div className="flex flex-1 flex-col items-center justify-center">
-        <p className="text-xs font-bold pt-6">ONLINE RESUME BUILDER</p>
-        <p className="text-3xl font-bold pt-2 w-5/12 text-center">
+        <p className="text-sm font-bold pt-[4rem] tracking-widest">
+          ONLINE RESUME BUILDER
+        </p>
+        <p className="text-5xl font-bold pt-2 w-7/12 text-center">
           Only 2% of resumes make it past the first round. Be in the top 2%
         </p>
-        <p className="text-xl pt-4 w-6/12 text-center">
+        <p className="text-xl pt-4 w-5/12 text-center">
           Use professional field-tested resume templates that follow the exact
           ‘resume rules’ employers look for. Easy to use and done within minutes
           - try now for free!
@@ -34,19 +36,23 @@ const DashboardMarkup = (props) => {
         <MyButton
           {...props}
           title="Create My Resume"
-          className="bg-blue-500 mt-4 p-3 pl-5 pr-5 rounded-md cursor-pointer button"
+          className="bg-blue-500 hover:bg-blue-600 mt-4 p-3 pl-5 pr-5 rounded-md cursor-pointer"
           textStyle="text-white font-bold"
           onPress={() => props.navigate("/resumes-templates")}
           loading={false}
         />
 
         <div className="flex flex-1 flex-row justify-center items-center">
-          <div className="bg-green-600 w-1 h-1 rounded-full mt-4 mr-2" />
+          <div className="bg-green-600 w-1.5 h-1.5 rounded-full animation-pulse mt-4 mr-2" />
           <p className="text-sm pt-4">0 resumes created today</p>
         </div>
       </div>
       <div className="flex flex-1 justify-center pt-4">
-        <img className="w-6/12 h-auto rounded-md" src={CVImage} alt="cv" />
+        <img
+          className="w-6/12 h-auto rounded-md"
+          src="https://s3.resume.io/cdn-cgi/image/width=770,height=350,dpr=1.5,fit=crop,gravity=top,quality=75,format=auto/uploads/local_template_image/image/488/persistent-resource/dublin-resume-templates.jpg"
+          alt="cv"
+        />
       </div>
     </div>
   );
