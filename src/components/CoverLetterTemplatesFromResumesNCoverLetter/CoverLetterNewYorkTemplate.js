34617f0c95f51a1b356886dd7a12134c2653a31d
Kashif Sheikh <kashifshaikh810@github.com>
2023-05-16 16:16:41 +0500

    Fix errors in CoverLetterNewYorkTemp And CoverLetterViennaTemp

--- src/components/CoverLetterTemplatesFromResumesNCoverLetter/CoverLetterNewYorkTemplate.js
+++ src/components/CoverLetterTemplatesFromResumesNCoverLetter/CoverLetterNewYorkTemplate.js
@@ -1,57 +1,51 @@
 import React from "react";
 
 const CoverLetterNewYorkTemplate = (props) => {
+  const { coverLetterData } = props;
+
   return (
     <div className="m-2 mt-3 overflow-hidden w-full">
       <div className="grid grid-cols-[81px_minmax(220px,_1fr)_100px] -space-x-[1.7rem]">
         <div className="w-7/12">
           <div className="flex flex-col items-end">
             <p className="text-[6px] font-semibold text-end uppercase">
-              {props?.coverLetterData?.fullName}
+              {coverLetterData?.fullName}
             </p>
             <p className="text-[3.5px] uppercase mt-1">
-              {props?.coverLetterData?.jobTitle}
+              {coverLetterData?.jobTitle}
             </p>
           </div>
 
           <div className="flex flex-col items-end mt-6">
             <p className="text-[3.5px] text-black font-bold">
-              {props?.coverLetterData?.hiringManagerName ||
-              props?.coverLetterData?.companyName
+              {coverLetterData?.hiringManagerName ||
+              coverLetterData?.companyName
                 ? `To`
                 : ``}
             </p>
-            <p className="text-[3.5px]">
-              {props?.coverLetterData?.hiringManagerName}
-            </p>
-            <p className="text-[3.5px]">
-              {props?.coverLetterData?.companyName}
-            </p>
+            <p className="text-[3.5px]">{coverLetterData?.hiringManagerName}</p>
+            <p className="text-[3.5px]">{coverLetterData?.companyName}</p>
           </div>
 
           <div className="flex flex-col items-end mt-6">
             <p className="text-[3.5px] text-black font-bold">
-              {props?.coverLetterData?.fullName ||
-              props?.coverLetterData?.jobTitle ||
-              props?.coverLetterData?.address ||
-              props?.coverLetterData?.email ||
-              props?.coverLetterData?.phone
+              {coverLetterData?.fullName ||
+              coverLetterData?.jobTitle ||
+              coverLetterData?.address ||
+              coverLetterData?.email ||
+              coverLetterData?.phone
                 ? `From`
                 : ``}
             </p>
-            <p className="text-[3.5px]">{props?.coverLetterData?.fullName}</p>
-            <p className="text-[3.5px]">{props?.coverLetterData?.jobTitle}</p>
-            <p className="text-[3.5px]">{props?.coverLetterData?.address}</p>
-            <p className="text-[3.5px]">{props?.coverLetterData?.phone}</p>
-            <p className="text-[3.5px] underline">
-              {props?.coverLetterData?.email}
-            </p>
+            <p className="text-[3.5px]">{coverLetterData?.fullName}</p>
+            <p className="text-[3.5px]">{coverLetterData?.jobTitle}</p>
+            <p className="text-[3.5px]">{coverLetterData?.address}</p>
+            <p className="text-[3.5px]">{coverLetterData?.phone}</p>
+            <p className="text-[3.5px] underline">{coverLetterData?.email}</p>
           </div>
         </div>
         <div className="w-6/12 h-[16rem] border-l border-[#a7a7a7] pl-2 pt-[1.4rem]">
-          <p className="text-[3.5px]">
-            {props?.coverLetterData?.letterDetails}
-          </p>
+          <p className="text-[3.5px]">{coverLetterData?.letterDetails}</p>
         </div>
       </div>
     </div>
--- src/components/CoverLetterTemplatesFromResumesNCoverLetter/CoverLetterViennaTemplate.js
+++ src/components/CoverLetterTemplatesFromResumesNCoverLetter/CoverLetterViennaTemplate.js
@@ -1,48 +1,46 @@
 import React from "react";
 
 const CoverLetterViennaTemplate = (props) => {
-  const {
-    fullName,
-    jobTitle,
-    address,
-    email,
-    phone,
-    companyName,
-    hiringManagerName,
-    letterDetails,
-  } = props?.coverLetterData;
+  const { coverLetterData } = props;
 
   return (
     <div className="flex w-[12rem] h-full flex-col pt-6 ml-3 pb-7 overflow-hidden">
       <div>
-        <p className="text-[10px] font-semibold">{fullName}</p>
-        <p className="text-[5px]">{jobTitle}</p>
+        <p className="text-[10px] font-semibold">{coverLetterData?.fullName}</p>
+        <p className="text-[5px]">{coverLetterData?.jobTitle}</p>
       </div>
 
       <div className="mt-3 w-8/12">
         <p className="text-[4px] font-bold">
-          {hiringManagerName || companyName ? `To:` : ``} {hiringManagerName}
-          {hiringManagerName && companyName ? `,` : ``} {companyName}
+          {coverLetterData?.hiringManagerName || coverLetterData?.companyName
+            ? `To:`
+            : ``}{" "}
+          {coverLetterData?.hiringManagerName}
+          {coverLetterData?.hiringManagerName && coverLetterData?.companyName
+            ? `,`
+            : ``}{" "}
+          {coverLetterData?.companyName}
         </p>
-        <p className="text-[4px]">{letterDetails}</p>
+        <p className="text-[4px]">{coverLetterData?.letterDetails}</p>
       </div>
 
       <div className="flex grid-cols-[200px_minmax(200px,_1fr)_100px] flex-1 justify-center items-end">
         <div className="flex flex-1 flex-row items-center flex-wrap">
-          <p className="text-[4px]">{address}</p>
-          {(address && phone) || email ? (
+          <p className="text-[4px]">{coverLetterData?.address}</p>
+          {(coverLetterData?.address && coverLetterData?.phone) ||
+          coverLetterData?.email ? (
             <div className="w-[1px] h-[1px] bg-black mr-[2px] ml-[2px]" />
           ) : (
             <></>
           )}
-          <p className="text-[4px]">{phone}</p>
-          {phone && email ? (
+          <p className="text-[4px]">{coverLetterData?.phone}</p>
+          {coverLetterData?.phone && coverLetterData?.email ? (
             <div className="w-[1px] h-[1px] bg-black mr-[2px] ml-[2px]" />
           ) : (
             <></>
           )}
 
-          <p className="text-[4px] underline">{email}</p>
+          <p className="text-[4px] underline">{coverLetterData?.email}</p>
         </div>
         <div className="flex flex-1 justify-end">
           <div className="w-9/12 h-1 bg-green-300" />
