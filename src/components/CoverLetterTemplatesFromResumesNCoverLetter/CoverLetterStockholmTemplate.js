93606931dd81b078bcede5660da55df048014e57
Kashif Sheikh <kashifshaikh810@github.com>
2023-05-16 16:20:42 +0500

    Fix error in CoverLetterStockholmTemp

--- src/components/CoverLetterTemplatesFromResumesNCoverLetter/CoverLetterStockholmTemplate.js
+++ src/components/CoverLetterTemplatesFromResumesNCoverLetter/CoverLetterStockholmTemplate.js
@@ -1,44 +1,47 @@
 import React from "react";
 
 const CoverLetterStockholmTemplate = (props) => {
-  const {
-    fullName,
-    jobTitle,
-    address,
-    email,
-    phone,
-    companyName,
-    hiringManagerName,
-    letterDetails,
-  } = props?.coverLetterData;
+  const { coverLetterData } = props;
 
   return (
     <div className="m-3 mr-0 overflow-hidden">
       <div>
-        <p className="text-[8px] font-bold">{fullName}</p>
-        <p className="text-[3px] relative -top-[2px]">{jobTitle}</p>
+        <p className="text-[8px] font-bold">{coverLetterData?.fullName}</p>
+        <p className="text-[3px] relative -top-[2px]">
+          {coverLetterData?.jobTitle}
+        </p>
       </div>
 
       <div className="grid gap-x-4 grid-cols-[120px_minmax(900px,_1fr)_100px] mt-4">
         <div>
-          <p className="text-[3px] leading-[7px]">{letterDetails}</p>
+          <p className="text-[3px] leading-[7px]">
+            {coverLetterData?.letterDetails}
+          </p>
         </div>
         <div>
           <p className="text-[3px] font-bold">
-            {companyName || hiringManagerName ? `To` : ``}
+            {coverLetterData?.companyName || coverLetterData?.hiringManagerName
+              ? `To`
+              : ``}
           </p>
-          <p className="text-[3px]">{companyName}</p>
-          <p className="text-[3px]">{hiringManagerName}</p>
+          <p className="text-[3px]">{coverLetterData?.companyName}</p>
+          <p className="text-[3px]">{coverLetterData?.hiringManagerName}</p>
 
           <div className="mt-2">
             <p className="text-[3px] font-bold">
-              {fullName || jobTitle || address || phone || email ? `From` : ``}
+              {coverLetterData?.fullName ||
+              coverLetterData?.jobTitle ||
+              coverLetterData?.address ||
+              coverLetterData?.phone ||
+              coverLetterData?.email
+                ? `From`
+                : ``}
             </p>
-            <p className="text-[3px]">{fullName}</p>
-            <p className="text-[3px]">{jobTitle}</p>
-            <p className="text-[3px]">{address}</p>
-            <p className="text-[3px]">{phone}</p>
-            <p className="text-[3px] text-blue-500">{email}</p>
+            <p className="text-[3px]">{coverLetterData?.fullName}</p>
+            <p className="text-[3px]">{coverLetterData?.jobTitle}</p>
+            <p className="text-[3px]">{coverLetterData?.address}</p>
+            <p className="text-[3px]">{coverLetterData?.phone}</p>
+            <p className="text-[3px] text-blue-500">{coverLetterData?.email}</p>
           </div>
         </div>
       </div>
