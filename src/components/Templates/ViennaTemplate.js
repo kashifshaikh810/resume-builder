aa73d7e9f9cd43e895b928b99a3e1bb8d7e7d442
Kashif Sheikh <kashifshaikh810@github.com>
2023-05-23 17:17:16 +0500

    Fix ViennaTemplate in AllTemplates screen

--- src/components/Templates/ViennaTemplate.js
+++ src/components/Templates/ViennaTemplate.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { MdOutlineAttachment } from "react-icons/md";
 
 import "./styles.css";
@@ -46,13 +46,23 @@ const ViennaTemplate = (props) => {
     disabledPreferences,
   } = props?.resumeData || {};
 
-  useEffect(() => {
-    props.setTotalPage(2);
-  }, [props]);
-
-  const pageOne = () => {
-    if (props.page === 1) {
-      return (
+  return (
+    <div
+      className="flex flex-col w-full h-[1200px] 
+      border-l border-gray-600 ml-5
+      max-[1029px]:pl-1 pl-10 pb-10 
+      unselectable
+      max-[768px]:border-none
+      max-[768px]:ml-0
+      max-[620px]:h-[900px]
+      max-[703px]:h-[880px]
+      max-[610px]:h-[830px]
+      max-[570px]:h-[780px]
+      max-[535px]:h-[730px]
+      max-[500px]:h-[680px]
+      "
+    >
+      <div className="flex bg-white w-full max-[1029px]:w-11/12 overflow-hidden h-[1100px] self-center rounded-md hover:default">
         <div className="p-10 w-full">
           <div className="flex flex-row w-full">
             {profileImage && (
@@ -696,90 +706,58 @@ const ViennaTemplate = (props) => {
               ) : (
                 ""
               )}
-            </div>
-          </div>
-        </div>
-      );
-    }
-  };
 
-  const pageTwo = () => {
-    if (props.page === 2) {
-      return (
-        <div className="flex flex-1 justify-center mt-5">
-          {referencesInputList?.length >= 1 ? (
-            <div className={internshipInputList?.length >= 1 ? `mt-5` : ``}>
-              <p className="font-bold">{referencesInput}</p>
+              {referencesInputList?.length >= 1 ? (
+                <div className={internshipInputList?.length >= 1 ? `mt-5` : ``}>
+                  <p className="font-bold">{referencesInput}</p>
 
-              {disabledPreferences ? (
-                <p className="text-[12px] text-black font-bold">
-                  Preferences available upon request
-                </p>
+                  {disabledPreferences ? (
+                    <p className="text-[12px] text-black font-bold">
+                      Preferences available upon request
+                    </p>
+                  ) : (
+                    ""
+                  )}
+                  {disabledPreferences === false &&
+                    referencesInputList &&
+                    referencesInputList.map((item, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className={`${
+                            referencesInputList?.length >= 2 && index >= 1
+                              ? `mt-5`
+                              : ``
+                          }`}
+                        >
+                          <p className="font-bold text-[12px] mt-1">
+                            {`${item?.referentFullName} ${
+                              item?.referentFullName && item?.company
+                                ? "from"
+                                : ""
+                            } ${item?.company}${item?.company}`}
+                          </p>
+                          <div className="flex flex-row items-center">
+                            <p className="text-[12px] mt-0.5">{item?.phone}</p>
+                            {item?.phone && item?.email ? (
+                              <div className="bg-gray-700 w-[2px] h-[2px] rounded-full ml-2 mr-2 mt-1" />
+                            ) : (
+                              ""
+                            )}
+                            <p className="text-[12px] mt-1 underline">
+                              {item?.email}
+                            </p>
+                          </div>
+                        </div>
+                      );
+                    })}
+                </div>
               ) : (
                 ""
               )}
-              {disabledPreferences === false &&
-                referencesInputList &&
-                referencesInputList.map((item, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className={`${
-                        referencesInputList?.length >= 2 && index >= 1
-                          ? `mt-5`
-                          : ``
-                      }`}
-                    >
-                      <p className="font-bold text-[12px] mt-1">
-                        {`${item?.referentFullName} ${
-                          item?.referentFullName && item?.company ? "from" : ""
-                        } ${item?.company}${item?.company}`}
-                      </p>
-                      <div className="flex flex-row items-center">
-                        <p className="text-[12px] mt-0.5">{item?.phone}</p>
-                        {item?.phone && item?.email ? (
-                          <div className="bg-gray-700 w-[2px] h-[2px] rounded-full ml-2 mr-2 mt-1" />
-                        ) : (
-                          ""
-                        )}
-                        <p className="text-[12px] mt-1 underline">
-                          {item?.email}
-                        </p>
-                      </div>
-                    </div>
-                  );
-                })}
             </div>
-          ) : (
-            ""
-          )}
+          </div>
         </div>
-      );
-    }
-  };
-
-  return (
-    <div
-      className="flex flex-col w-full h-[930px] 
-      border-l border-gray-600 ml-5
-      max-[1029px]:pl-1 pl-10 pb-10 
-      unselectable
-      max-[768px]:border-none
-      max-[768px]:ml-0
-      max-[620px]:h-[900px]
-      max-[703px]:h-[880px]
-      max-[610px]:h-[830px]
-      max-[570px]:h-[780px]
-      max-[535px]:h-[730px]
-      max-[500px]:h-[680px]
-      "
-    >
-      <div className="flex bg-white w-full max-[1029px]:w-11/12 overflow-hidden h-[900px] self-center rounded-md hover:default">
-        {/* pageOne */}
-        {pageOne()}
-
-        {/* pageTwo */}
-        {pageTwo()}
       </div>
     </div>
   );
